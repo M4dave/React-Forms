c881bef4d42f9a62b0b7ae8499d3d32bf66b4d97
M4dave <davidjohncruz@gmail.com>
2024-05-16 23:26:16 -0400

    updated css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Authenticate from "./components/Authenticate";
 import SignUpForm from "./components/SignUpForm";
 import { useState } from "react";
-import "./App.css";
+// import "./App.css";
 
 function App() {
   const [token, setToken] = useState(null);
